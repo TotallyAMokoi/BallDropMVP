98f7c614b101a2064110dc766e38af7b07526dae
TotallyAMokoi <ashleighbulcock@gmail.com>
2017-05-04 12:00:20 +1000

    Added everything

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,29 @@
+﻿//Edited from https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/following-player-camera
+//Holy shit bro thank you
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        Vector3 curPosition = new Vector3(0, player.transform.position.y, 0) + offset;
+        transform.position = curPosition;
+    }
+}
---
+++ Assets/imbeggingyou.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class imbeggingyou : MonoBehaviour { private Vector3 screenPoint; private Vector3 offset; private float _lockedXPosition;
+
+     void OnMouseDown() {
+        //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); // I removed this line to prevent centring 
+        _lockedXPosition = screenPoint.x;
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        
+         
+         
+               
+     }
+     
+     void OnMouseDrag() 
+     { 
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        curPosition.y = _lockedXPosition;
+        transform.position = curPosition;
+     }
+     
+     void OnMouseUp()
+     {
+        Cursor.visible = true;
+     }
+}
